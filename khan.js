422e369933fe016670759ba57e57ceba64dfc3cd
Rafi <rafi@rafikkhan-latitude-5280.bbrouter>
2023-02-06 16:26:33 +0530

    my first comit

---
+++ khan.js
@@ -0,0 +1,217 @@
+
+
+
+// //2
+// const a = 91
+// if (a >= 80) {
+//     console.log("merit me")
+// }
+// else if (a >= 65 && a <= 79) {
+//     console.log("first division")
+
+// } else if (a >= 40 && a <= 64) {
+//     console.log("second division")
+// } else if (a <= 39) {
+//     console.log("fail")
+// }
+
+// //1
+// (a >= 80) ? console.log("merit me") : console.log("second divison");
+
+
+// //3
+// let d = 45;
+// let f = 30;
+// if (d > f) {
+//     console.log("max")
+// } else if (f > d) {
+//     console.log("small")
+// }
+
+// //4
+// let y = 15;
+// var u = 43;
+// let b = 22;
+// if (y < u) {
+//     console.log("max 2")
+// } else if (u > b) {
+//     console.log("small 2")
+// } else if (b < y) {
+//     console.log("small")
+// }
+
+
+//10
+//age chek
+
+// let year = 2004;
+// let today = 2023
+// let age = today - year;
+// console.log(age)
+
+// //11
+// let yea = 2023;
+// let bitu = 2000;
+// let rustm = 2015;
+// let devender = 2008;
+
+// let r = yea-bitu;
+// let k = yea-rustm;
+// let l = yea-devender;
+
+// if(r>=k && r>=l){
+// console.log("bitu")
+// }else if(k>=l && k>=r){
+// console.log("rustm")
+// }else{
+//     console.log("devender")
+// }
+
+//  1
+//  let u = 3;
+//  let i = 4;
+// let p = u+i
+
+//  console.log(p)
+
+//  let e = 3;
+//  let n = 4;
+// let a = u-i
+
+//  console.log(a)
+
+//  let v = 3;
+//  let b = 4;
+// let x = u*i
+
+//  console.log(x)
+
+//  let g = 3;
+//  let d = 4;
+// let l = u/i
+
+//  console.log(l)
+
+ //2
+//  let f = 5;
+//  if(f%2===1){
+// console.log("even")
+//  }else {
+//     console.log("odd")
+//  }
+
+ //3
+//let h = (prompt("my name is rafik"))
+//console.log(h)
+
+
+//6
+// let I =(window.alert("hellow world"));
+
+// //7
+// let user = null;
+// let k = 0;
+// let y = undefined;
+// let Q = NaN;
+// let B = false;
+// let E = "";
+// if(user){
+// console.log("truthy")
+// }else{
+//     console.log("falsy")
+// }
+// if(k){
+//     console.log("truthy")
+//     }else{
+//         console.log("falsy")
+//     }
+//     if(y){
+//         console.log("truthy")
+//         }else{
+//             console.log("falsy")
+//         }
+//         if(Q){
+//             console.log("truthy")
+//             }else{
+//                 console.log("falsy")
+//             }
+//             if(B){
+//                 console.log("truthy")
+//                 }else{
+//                     console.log("falsy")
+//                 }
+//                 if(E){
+//                     console.log("truthy")
+//                     }else{
+//                         console.log("falsy")
+//                     }
+
+//8
+// let W =confirm("do you want to play");
+// if(W){
+//     console.log("yes")
+// }else{
+//     console.log("no")
+// }
+
+
+
+// //9
+// let h = 6;
+// if(h>=5 && h>=3){
+// console.log("big family")
+// }else{
+//  console.log("small")   
+// }         
+
+//10
+// for(let a = 10 ;a>=1;a--){
+//     console.log(a)
+// }
+// console.log("hhhhhhhhhhhh ")
+// for(let b = 1 ;b<=10;b++){
+//     console.log(b)
+// }
+
+//1
+// for(let j = 1 ; j<= 10;j++){
+//     console.log(j)
+//     console.log("hellow")
+// }
+
+// //3
+// for(let o=0;o<=20;o++){
+// if (o%2==0) {
+//     console.log(o);
+// }
+// }
+//4
+for(let h = 0;h<=40;h++){
+if(h%4==0){
+console.log(h)
+}
+if(h%8==0){
+    console.log(h);
+}
+}
+
+//1
+// for(let O = 1;O<=100;O++){
+// if(O%2==0){
+//     console.log("even",O)
+// }
+// }
+
+//2
+for(const A=40;A<=60;A++){
+if(A%5==0){
+console.log(A)
+}
+}
+
+//3
+for(const S = 0;S<=50;S++){
+if(S%5==0){
+console.log(S)
+}
+}
