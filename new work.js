b814cc1514142a5afff344e4ebc5f538dc10106e
Rafi <rafikkhanpeeh@gmail.com>
2023-02-15 22:42:58 +0530

    new file

---
+++ new work.js
@@ -0,0 +1,42 @@
+// // //1
+// let frenc1 = 60
+// let burger2 = 50
+// let chowmin3 = 100
+// let manchuriyn4 = 80
+// let cokes5 = 50
+
+// let french = Number((prompt("60 per piece do you want to order")))
+// let burger = Number((prompt("50 per piece do you want  to order")))
+// let chowmin = Number((prompt("100 per piece do you want to order")))
+// let manchuriyn = Number((prompt("80 per piece do you want to order")))
+// let cokes = Number((prompt("50 per piece do you want to order")))
+
+// console.log(frenc1 * french + burger * burger2 + chowmin * chowmin3 + manchuriyn4 * manchuriyn + cokes5 * cokes)
+
+//2
+// for(let j=7;j>=1;j--){
+// ans=""
+// for(let y=1;y<=j;y++){
+// ans=ans+"&#"
+// }
+// console.log(ans)
+// }
+
+//8
+// let h=4
+// if(h<=5 &! h<=8){
+// console.log(yes)
+// }else{
+// console.log(no)    
+// }
+
+//10
+let g="Wecode"
+if(g.length<10){
+console.log(g)
+for(let y=1;y<=8;y++){
+console.log(y)
+}
+}else{
+  console.log(no)  
+}
